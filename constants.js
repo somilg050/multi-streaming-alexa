57d65f8374fa9a6d6ad769912d1a526e620d8090
Somil Gupta <somilg050@gmail.com>
2023-06-09 23:41:16 +0530

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,398 @@
+exports.audioData = [
+  [
+    'https://www.dropbox.com/s/rr2m6vwxbblw3vl/VSN%20-%20No%20Reason.mp3?raw=1',
+    'VSN - No Reason'
+  ],
+  [
+    'https://www.dropbox.com/s/rv2ijoneiyn4xt0/2am%20-%20FALL4ME.mp3?raw=1',
+    '2am - FALL4ME'
+  ],
+  [
+    'https://www.dropbox.com/s/xdq3q7odac88cth/4ever%20Song%20Final.wav?raw=1',
+    '4ever - Don Dotta'
+  ],
+  [
+    'https://www.dropbox.com/s/3szvhzrws3u9suq/22%20-%20Music%20Ground%20-%20Young%20NVNV%20-%20My%20Everything%20prod%20by%20Vybe%20Beatz.mp3?raw=1',
+    'Young NV - My Everythang'
+  ],
+  [
+    'https://www.dropbox.com/s/eljhqky7un41bgw/A1-Call%20On%20You.mp3?raw=1',
+    'A1 - Call On You'
+  ],
+  [
+    'https://www.dropbox.com/s/t92f3p0xndlqur0/Adley%20Beaubrun%20-%20Encount%27rs.mp3?raw=1',
+    "Adley Beaubrun - Encount'rs "
+  ],
+  [
+    'https://www.dropbox.com/s/xi2lwnyad7rl53t/Aliyah%20Lanay%20-%2022.mp3?raw=1',
+    'Aliyah Lanay - 22 '
+  ],
+  [
+    'https://www.dropbox.com/s/pd3nwhrwx42s3em/Amaal%20Nuux%2C%20Syd%20-%20Lullaby%20%28feat.%20Syd%29.mp3?raw=1',
+    'Amaal Nuux, Syd - Lullaby'
+  ],
+  [
+    'https://www.dropbox.com/s/quqak8wi2z1xpnb/Am%C4%81re%20-%20Talk%20To%20Me.mp3?raw=1',
+    'Amāre - Talk To Me'
+  ],
+  [
+    'https://www.dropbox.com/s/8tm7bbi71rhmm6o/Ashton%20-%20Love%20Me%20Not.mp3?raw=1',
+    'Ashton - Love Me Not'
+  ],
+  [
+    'https://www.dropbox.com/s/0zu54o1l4tlm66s/AZU%20CAPIANO%20-%20RAINDROPS.mp3?raw=1',
+    'Azu Capiano - Raindrops'
+  ],
+  [
+    'https://www.dropbox.com/s/dylemrvw49lqff9/Broadcast%20-%20dj-drop.mp3?raw=1',
+    'Broadcast - dj-drop'
+  ],
+  [
+    'https://www.dropbox.com/s/crrhm4g7jao1ouc/BURY%2C%20SUNDERWORLD%20-%20THE%20SUN.mp3?raw=1',
+    'Bury, Sunderworld - The Sun'
+  ],
+  [
+    'https://www.dropbox.com/s/4kyh6coedyq4brw/Caleb%20Cruise%20-%20love%20all.mp3?raw=1',
+    'Caleb Cruise - love all'
+  ],
+  [
+    'https://www.dropbox.com/s/twnfd3ltkih6dcw/Clay%20Captcha%20-%20Do%20Not%20Disturb.mp3?raw=1',
+    'Clay Captcha - Do Not Disturb'
+  ],
+  [
+    'https://www.dropbox.com/s/amm7sziejycemvc/BRANDEUS%2C%20RIZ%20-%20SINNER.mp3?raw=1',
+    'Brandeus, Riz - Sinner'
+  ],
+  [
+    'https://www.dropbox.com/s/elceig9joxrzon2/Cayo%20Coco%20-%20Strangers.mp3?raw=1',
+    'Cayo Coco - Strangers'
+  ],
+  [
+    'https://www.dropbox.com/s/f3x8a09do543v2f/D.%20Alexander%20-%20Need%20Us.mp3?raw=1',
+    'D. Alexander - Need Us'
+  ],
+  [
+    'https://www.dropbox.com/s/rmerfw1tsb6qsrw/Caleb%20Cruise%20-%20rich.mp3?raw=1',
+    'Caleb Cruise - rich'
+  ],
+  [
+    'https://www.dropbox.com/s/7lyscxu5y0mfgq6/Da%20Candy%2C%20Kodamilo%20-%20Head%20in%20the%20clouds.mp3?raw=1',
+    'Da Candy, Kodamilo - Head in the clouds'
+  ],
+  [
+    'https://www.dropbox.com/s/ja40jt98jv7lbh4/Don%20Dotta%20-%20ice%20so%20cold%20radio.mp3?raw=1',
+    'Don Dotta - ice so cold radio'
+  ],
+  [
+    'https://www.dropbox.com/s/18sk3srm77t51c5/Dree%20Rivers%2C%20GBABY%20-%2088STATIC.mp3?raw=1',
+    'Dree Rivers, GBABY - 88STATIC'
+  ],
+  [
+    'https://www.dropbox.com/s/4nea4l6urlz8p12/Drop%20-%20calling%20out%20new%20artist.mp3?raw=1',
+    'Drop - calling out new artist'
+  ],
+  [
+    'https://www.dropbox.com/s/n9hmi49hns33ro9/Bri%20333%20-%20Potentia.mp3?raw=1',
+    'Bri 333 - Potentia'
+  ],
+  [
+    'https://www.dropbox.com/s/zybrzz68jdxxdok/Chris%20Grey%20-%20BURNING%20IN%20DESIRE.mp3?raw=1',
+    'Chris Grey - Burning in desire'
+  ],
+  [
+    'https://www.dropbox.com/s/3t0t21lur1ns439/Darci%20-%20Tell%20Me%20When.mp3?raw=1',
+    'Darci - Tell Me When'
+  ],
+  [
+    'https://www.dropbox.com/s/qexzexa0q2wnqm8/Elliott%20Bali%20-%20Woman%20Of%20My%20Dreams.mp3?raw=1',
+    'Elliott Bali - Woman Of My Dreams'
+  ],
+  [
+    'https://www.dropbox.com/s/wkaiwis0y17o5i4/Eloe%20-%201%2C000%20Times.mp3?raw=1',
+    'Eloe - 1,000 Times'
+  ],
+  [
+    'https://www.dropbox.com/s/6an0tl8eyewkmb6/Am%C4%81re%20-%20US.mp3?raw=1',
+    'Amāre - US'
+  ],
+  [
+    'https://www.dropbox.com/s/vc2gtmq3ofwn7yh/Damaged.%20-%20Ride.mp3?raw=1',
+    'Damaged. - Ride'
+  ],
+  [
+    'https://www.dropbox.com/s/fxoylpvd4y5gbv3/Eloy%2C%20Michael%20Miller%20-%20I%20Know.mp3?raw=1',
+    'Eloy, Michael Miller - I Know'
+  ],
+  [
+    'https://www.dropbox.com/s/ozvztg134ei42o2/Gena%20tew%20%20-%20I%20live%20for%20you.mp3?raw=1',
+    'Gena tew - I live for you'
+  ],
+  [
+    'https://www.dropbox.com/s/csjgk98xwsdwlio/Cayo%20Coco%20-%20Somebody%20Else.mp3?raw=1',
+    'Cayo Coco - Somebody Else'
+  ],
+  [
+    'https://www.dropbox.com/s/5ah22ittyd6mhhf/Don%20Dotta%20-%20not%20bad%20at%20all.mp3?raw=1',
+    'Don Dotta - not bad at all'
+  ],
+  [
+    'https://www.dropbox.com/s/hxlpac3pgksiak1/Gray%2C%20Hrtbrkfever%20-%20Out%20My%20Mind.mp3?raw=1',
+    'Gray, Hrtbrkfever - Out My Mind'
+  ],
+  [
+    'https://www.dropbox.com/s/ow7vjrw2hjrcfyz/Hrtbrkfever%20-%20DRAMA%20x2.mp3?raw=1',
+    'Hrtbrkfever - DRAMA x2'
+  ],
+  [
+    'https://www.dropbox.com/s/1l625pnd3lsv1av/JAIoftheRise%20-%20FULL%20MOON.mp3?raw=1',
+    'JAIoftheRise - FULL MOON'
+  ],
+  [
+    'https://www.dropbox.com/s/mdsa70w83ckkndd/Caleb%20Cruise%20-%20stay%20close.mp3?raw=1',
+    'Caleb Cruise - stay close'
+  ],
+  [
+    'https://www.dropbox.com/s/tuxkipko62npq1c/Don%20Dotta%20-%20No%20Time%20prod%20OBE%20Beatz.mp3?raw=1',
+    'Don Dotta - no time'
+  ],
+  [
+    'https://www.dropbox.com/s/n6mmn6ju4uxm09r/TazDemar%20-%20Runaway%20Love.mp3?raw=1',
+    'TazDemar - Runaway Love'
+  ],
+  [
+    'https://www.dropbox.com/s/56ejfbzb7otdxlp/Aliyah%20Lanay%20-%20Heavy%20on%20it.mp3?raw=1',
+    'Aliyah Lanay - Heavy on it'
+  ],
+  [
+    'https://www.dropbox.com/s/syk1uwq9kx1hdf3/Josh%20Waters%20-%20Gelato.mp3?raw=1',
+    'Josh Waters - Gelato'
+  ],
+  [
+    'https://www.dropbox.com/s/lsn3t4ini6tvduq/Geremiahhh%20-%20REAL%20NAME.mp3?raw=1',
+    'Geremiahhh - REAL NAME'
+  ],
+  [
+    'https://www.dropbox.com/s/53pnra5gdyp3hwk/JusRay%20-%20Light%20It%20Up.mp3?raw=1',
+    'JusRay - Light It Up'
+  ],
+  [
+    'https://www.dropbox.com/s/rqu3ks176y0114u/Don-Dotta%20-%20Coolin.mp3?raw=1',
+    'Don Dotta - Coolin'
+  ],
+  [
+    'https://www.dropbox.com/s/5yeoa1ak3ymsnoq/Keont%C3%A9%20-%20B2U.mp3?raw=1',
+    'Keonté - B2U'
+  ],
+  [
+    'https://www.dropbox.com/s/mdg2jlobgb84czv/JEFFREY%20OLIVER%20-%20Not%20my%20friends.mp3?raw=1',
+    'JEFFREY OLIVER - Not my friends'
+  ],
+  [
+    'https://www.dropbox.com/s/if5qg14b1nvnzfw/LazyMane%20and%20Kosher%20Beats%20-%20The%20Humble.mp3?raw=1',
+    'LazyMane and Kosher Beats  - The Humble'
+  ],
+  [
+    'https://www.dropbox.com/s/35oxm4v5elxmaig/TazDemar%20-%20Romeo.mp3?raw=1',
+    'TazDemar - Romeo'
+  ],
+  [
+    'https://www.dropbox.com/s/bjetuirwuiadrj9/Tre%20Ward%20-%20FLOWERS%20AT%20YOUR%20DOOR.mp3?raw=1',
+    'Tre Ward - FLOWERS AT YOUR DOOR'
+  ],
+  [
+    'https://www.dropbox.com/s/4npd9xucfm2v0bf/Jude%20Love%20-%20Waves.mp3?raw=1',
+    'Jude Love - Waves'
+  ],
+  [
+    'https://www.dropbox.com/s/6kg7y7gs96pq3hl/Lester%20Sanchez%20-%20Dear%20Mary%20Jane.mp3?raw=1',
+    'Lester Sanchez - Dear Mary Jane'
+  ],
+  [
+    'https://www.dropbox.com/s/zy82vlx713ozdvs/Johnny%20Rain%20-%20...Fuck.mp3?raw=1',
+    'Johnny Rain - Fuck'
+  ],
+  [
+    'https://www.dropbox.com/s/27gsmlj7w85jkic/Lola%20Ladae%27%20-%20You%20Deserve%20It.mp3?raw=1',
+    "Lola Ladae' - You Deserve It"
+  ],
+  [
+    'https://www.dropbox.com/s/erwdnk2grjiwgm2/Marcus%20Law%20-%20Rejection.mp3?raw=1',
+    'Marcus Law - Rejection'
+  ],
+  [
+    'https://www.dropbox.com/s/i6f7q71n9tfzsjx/Landstrip%20Chip%2C%20Baby%20Tate%20-%20Wrong%20Way%20%28Remix%29%20%5Bfeat.%20Baby%20Tate%5D.mp3?raw=1',
+    'Landstrip Chip, Baby Tate - Wrong Way (Remix) [feat. Baby Tate]'
+  ],
+  [
+    'https://www.dropbox.com/s/ojdwyq9qe5qi0xt/love%20me%2012-13-22.mp3?raw=1',
+    'Don Dotta - Love me'
+  ],
+  [
+    'https://www.dropbox.com/s/yjaauef4no5fema/Mere%20Raj%20-%20inmyhead.mp3?raw=1',
+    'Mere Raj - inmyhead'
+  ],
+  [
+    'https://www.dropbox.com/s/m107yxhkzm1gnr3/K%20Alex%20-%20Hope%20Gone.mp3?raw=1',
+    'K Alexander - Hope Gone'
+  ],
+  [
+    'https://www.dropbox.com/s/2auljt3ryp68i0d/Marcus%20Law%20-%20We%20Out%20%28Prod.%20By%20PDUBtheProducer%29.mp3?raw=1',
+    'Marcus Law - We Out prod PDUBtheProducer'
+  ],
+  [
+    'https://www.dropbox.com/s/2qawq9vqawcx3c9/millkzy%2C%20yungcapi%20-%20to%20whom%20it%20may%20concern.mp3?raw=1',
+    'millkzy, yungcapi -… it may concern'
+  ],
+  [
+    'https://www.dropbox.com/s/rfohqn8ekywzrg2/Lost%20Flyer%20-%20Over%20It.mp3?raw=1',
+    'Lost Flyer - Over It'
+  ],
+  [
+    'https://www.dropbox.com/s/z3ygn540nlqup5p/Keont%C3%A9%20-%20OVER%20U.mp3?raw=1',
+    'Keonté - OVER U'
+  ],
+  [
+    'https://www.dropbox.com/s/6ni9dytlmcdhi9j/LinaDrae%20-%20Rich.mp3?raw=1',
+    'LinaDrae - Rich'
+  ],
+  [
+    'https://www.dropbox.com/s/jz9svahmnw03kdr/Mentiras%2C%20Jake%20Hope%20-%20Things.mp3?raw=1',
+    'Mentiras, Jake Hope - Things'
+  ],
+  [
+    'https://www.dropbox.com/s/ps0nd5kr84cnnl2/mirrors%20-%20Fever.mp3?raw=1',
+    'mirrors - Fever'
+  ],
+  [
+    'https://www.dropbox.com/s/51fck9enfnuf099/Melion%20Music%20-%20Trouble.mp3?raw=1',
+    'Melion Music - Trouble'
+  ],
+  [
+    'https://www.dropbox.com/s/50r9r0nwt9pl0cu/BlueWheezy%20-%20Reign%20April.mp3?raw=1',
+    'BlueWheezy - Reign April'
+  ],
+  [
+    'https://www.dropbox.com/s/9g51fhb5acfdd85/Mishaal%20Tamer%20-%20CIGARETTE.mp3?raw=1',
+    'Mishaal Tamer - CIGARETTE'
+  ],
+  [
+    'https://www.dropbox.com/s/1w2qzxnqtdd089n/Moslikely%20-%20Moving%20Up.mp3?raw=1',
+    'Moslikely - Moving Up'
+  ],
+  [
+    'https://www.dropbox.com/s/smjoti971fq0l6v/Music%20Ground%20-%20Announcement.mp3?raw=1',
+    'Music Ground - Announcement'
+  ],
+  [
+    'https://www.dropbox.com/s/34u03cv1mdk9r8m/Music%20Ground%20-%20tag.mp3?raw=1',
+    'Music Ground - Tag'
+  ],
+  [
+    'https://www.dropbox.com/s/ajrdevudanji5m0/Not.Fay%20-%20One.mp3?raw=1',
+    'Not.Fay - One'
+  ],
+  [
+    'https://www.dropbox.com/s/lra48docsu9rbar/Parker%20Sharpe%20-%2097%20Baby.mp3?raw=1',
+    'Parker Sharpe - 97 Baby'
+  ],
+  [
+    'https://www.dropbox.com/s/r8lg4fn0mwnvbks/PassionD%20-%20Best%204%20You.mp3?raw=1',
+    'PassionD - Best 4 You'
+  ],
+  [
+    'https://www.dropbox.com/s/dayaovnhcyw1ld7/Penthouse%20livin%20ft%20doe%20boy%20kap.mp3?raw=1',
+    'Doe boy kap - Penthouse Livin '
+  ],
+  [
+    'https://www.dropbox.com/s/5ewqrl3ovl19pkt/Pino%20-%20CONCEITED.mp3?raw=1',
+    'Pino - CONCEITED'
+  ],
+  [
+    'https://www.dropbox.com/s/c75ok0qi8wvgt8x/Promenade%20-%20Anything.mp3?raw=1',
+    'Promenade - Anything'
+  ],
+  [
+    'https://www.dropbox.com/s/ux55s8bvtxuqflt/PD%20ft%20%28Want%20In%29.mp3?raw=1',
+    'PD ft (Want In)'
+  ],
+  [
+    'https://www.dropbox.com/s/j48gcbn74ftgv3l/Razi%20-%20Home.mp3?raw=1',
+    'Razi - Home'
+  ],
+  [
+    'https://www.dropbox.com/s/exd2nj40bwx6r22/P%20Dada%20Style.mp3?raw=1',
+    'P Dada Style'
+  ],
+  [
+    'https://www.dropbox.com/s/tsqsyy4fs5l2hrv/Reagan%20Kristin%20-%20Have%20You%20Ever.mp3?raw=1',
+    'Reagan Kristin - Have You Ever'
+  ],
+  [
+    'https://www.dropbox.com/s/uccyh61edwcr9i2/ready%20or%20not%2012-28-22.mp3?raw=1',
+    'Don Dotta - ready or not ft. Swae Lee'
+  ],
+  [
+    'https://www.dropbox.com/s/iebmc62tvnicawe/Reylovesu%20-%202%20Much%204%20Love.mp3?raw=1',
+    'Reylovesu - 2 Much 4 Love'
+  ],
+  [
+    'https://www.dropbox.com/s/euq0g7q7aq1j3m9/REALLIFEALWAYS%20-%20BOUND%20TO.mp3?raw=1',
+    'REALLIFEALWAYS - BOUND TO'
+  ],
+  [
+    'https://www.dropbox.com/s/9rw5xrwwzt5yg7h/Renn%20Olympus%2C%20Twelve%27len%20-%20Sweatpants%20%26%20Diamonds.mp3?raw=1',
+    "Renn Olympus, Twelve'len - Sweatpants and Diamonds"
+  ],
+  [
+    'https://www.dropbox.com/s/d2beky2l6shl9v2/RIZ%20-%20Nightmares.mp3?raw=1',
+    'RIZ - Nightmares'
+  ],
+  [
+    'https://www.dropbox.com/s/nn0n5at4qrlctok/Savv%20-%20fortunes..mp3?raw=1',
+    'Savv - fortunes'
+  ],
+  [
+    'https://www.dropbox.com/s/cj7xyi6n1ie1jgw/T.C.%20-%20Do%20What%20We%20Do.mp3?raw=1',
+    'T.C. - Do What We Do'
+  ],
+  [
+    'https://www.dropbox.com/s/wjhbjmdypgpl0hp/TazDemar%20-%20Love%20Ecstasy.mp3?raw=1',
+    'TazDemar - Love Ecstasy'
+  ],
+  [
+    'https://www.dropbox.com/s/dt3me1k1qez4p2z/Pino%2C%20Avi%20Roy%20-%20WILD.mp3?raw=1',
+    'Pino, Avi Roy - WILD'
+  ],
+  [
+    'https://www.dropbox.com/s/io615pibccfb9ew/Tee%20Melly%20-%2030%20Minutes.mp3?raw=1',
+    'Tee Melly - 30 Minutes'
+  ],
+  [
+    'https://www.dropbox.com/s/eeg562gtce8qftq/Trey%20Qua%20-%20Tel%20Aviv.mp3?raw=1',
+    'Trey Qua - Tel Aviv'
+  ],
+  [
+    'https://www.dropbox.com/s/rv3jty8zfmzk970/Twiggy%20Will%20-%20One%20Sided.mp3?raw=1',
+    'Twiggy Will - One Sided'
+  ],
+  [
+    'https://www.dropbox.com/s/0t4ze6auosk4kva/Reylovesu%2C%20Juicy%20J%2C%20Killers%20To%20Lovers%20-%20Hustling%20%26%20Grinding%20%23%20Come%20As%20You%20Are%20-%20Live.mp3?raw=1',
+    'Reylovesu, Juicy J, Killers to lovers - Come As You Are - Live'
+  ],
+  [
+    'https://www.dropbox.com/s/4vcvk3x5ijybtlt/RIZ%20-%20Therapy.mp3?raw=1',
+    'RIZ - Therapy'
+  ],
+  [
+    'https://www.dropbox.com/s/jnbkfe1dzxevh68/VSN%20Black%20-%20Deja%20Vu.mp3?raw=1',
+    'VSN Black - Deja Vu'
+  ],
+  [
+    'https://www.dropbox.com/s/5nk2a1n27xk72yd/Young%20Bankroll%20-%20Right%20Hea.mp3.mp3?raw=1',
+    'Young Bankroll - Right Hea'
+  ],
+  [
+    'https://www.dropbox.com/s/0lt2d15gj191geq/XANDi%20-%20Say%20My%20Name.mp3?raw=1',
+    'XANDi - Say My Name'
+  ]
+];
